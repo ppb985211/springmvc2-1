6a35164b1c34dab7308ba7cbd27da9b3ac4cc609
ppb <ppb985211@163.com>
2018-09-20 20:36:03 +0800

    SpringMvc  SpringJdbc实现分页

--- springmvc2-6/src/main/java/cap/dao/impl/AdminDAOImpl.java
+++ springmvc2-6/src/main/java/cap/dao/impl/AdminDAOImpl.java
@@ -7,9 +7,10 @@ import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
-    @Repository(value = "adminDAO")
+@Repository(value = "adminDAO")
     public class AdminDAOImpl implements AdminDAO {
         private JdbcTemplate jdbcTemplate;
 
@@ -23,5 +24,18 @@ import javax.annotation.Resource;
             String sql = "select * from admin where username=? and password=?";
             return this.jdbcTemplate.queryForObject(sql, new Object[]{admin.getUsername(), admin.getPassword()}, ParameterizedBeanPropertyRowMapper.newInstance(Admin.class));
         }
+
+    @Override
+    public List<Admin> findByPage(int pageNo, int pageSize) {
+        String sql = "select * from admin limit ?,?";
+        int start = (pageNo - 1) * pageSize;
+        return this.jdbcTemplate.query(sql, new Object[]{start, pageSize}, ParameterizedBeanPropertyRowMapper.newInstance(Admin.class));
     }
+
+    @Override
+    public int getTotalCount() {
+        String sql = "select count(*) from admin";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+}
 
